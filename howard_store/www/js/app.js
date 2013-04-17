89432650f655858d2ffb4ac0eab0a88de6d8215c
Mark Heflin <mheflin@gmail.com>
2013-04-17 19:20:36 -0400

    Revert "Heflin's app"
    This reverts commit 05fcc7e64d1a0c34c749e371686977950bd97938.

--- howard_store/www/js/app.js
+++
@@ -1,7 +0,0 @@
-function updateWidthAndHeight() {
-    document.getElementById('widthDiv').innerHTML = 'width: ' + window.innerWidth + 'px';
-    document.getElementById('heightDiv').innerHTML = 'height: ' + window.innerHeight + 'px';
-}
-
-document.addEventListener('orientationchange', updateWidthAndHeight, false);
-
--- howard_store/www/js/books.js
+++
@@ -1,7 +0,0 @@
-function updateWidthAndHeight(){
-    document.getElementById('widthDiv').innerHTMl ='width: ' + window.innerWidth + 'px';
-    document.getElementById('heightDiv').innerHTMl ='height: ' + window.innerHeight + 'px';
-}
-
-
-document.addEventListener('orientationchange', updateWidthAndHeight, false);
