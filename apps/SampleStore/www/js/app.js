dcfee4ac7fd38857eea06c1e07686a978cdea480
HC <student@GBTC441001DT.local>
2013-04-17 20:28:57 -0400

    Added Actionsheet button

--- apps/SampleStore/www/js/app.js
+++ apps/SampleStore/www/js/app.js
@@ -3,5 +3,24 @@ function updateWidthAndHeight() {
     document.getElementById('heightDiv').innerHTML = 'height: ' + window.innerHeight + 'px';
 }
 
+function setupActionSheet(callback) {
+    var actionSheet = window.plugins.actionSheet;
+    var options = {
+    title: 'Title',
+    items: ['Foo', 'Bar']
+    };
+    
+    // Basic with title
+    actionSheet.create({title: 'Menu', items: ['New releases', 'My Cart']}, callback);
+
+}
+
 document.addEventListener('orientationchange', updateWidthAndHeight, false);
 
+var button = document.getElementById("menubutton");
+button.addEventListener('click', function(e) {
+                        setupActionSheet( function (buttonValue, buttonIndex) {
+                            console.log('Button Value: ' + buttonValue);
+                            console.log('Button Index: ' + buttonIndex);
+                        });
+}, false);
